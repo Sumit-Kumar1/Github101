20448b30f145ad51d5aa6499a4749bf1c1b713a9
Sumit kumar <56474834+Sumit-Kumar7796@users.noreply.github.com>
2021-03-17 17:11:24 +0530

    cleaned follow player script

--- SliderBox/Assets/Scripts/followPlayer.cs
+++ SliderBox/Assets/Scripts/followPlayer.cs
@@ -4,13 +4,8 @@ public class followPlayer : MonoBehaviour
 {
     public Transform player;
     public Vector3 offset;
-    public Camera Cam;
-    private void Start() {
-        Cam = GetComponent<Camera>();
-    }
-    // Update is called once per frame
     void Update()
     {
-        Cam.transform.position = player.position + offset;    // The camera is positioned to player position
+        transform.position = player.position + offset;    // The camera is positioned to player position
     }
 }
